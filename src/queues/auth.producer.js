080c409b45f75f52b53ee0138b49bf7645d51e46
vmdt <vmdt03@gmail.com>
2024-09-26 16:03:09 +0700

    [feat]: setting bullmq and logic handle order job

--- src/queues/auth.producer.js
+++ src/queues/auth.producer.js
@@ -1,22 +1,21 @@
 const { createConnection } = require("./connection");
 
 const publishDirectMessage = async (
-    channel,
-    exchangeName,
-    routingKey,
-    message
+	channel,
+	exchangeName,
+	routingKey,
+	message,
 ) => {
-    try {
-        if (!channel)
-            channel = await createConnection();
+	try {
+		if (!channel) channel = await createConnection();
 
-        await channel.assertExchange(exchangeName, 'direct');
-        channel.publish(exchangeName, routingKey, Buffer.from(message));
-    } catch (error) {
-        console.log('Publish direct message error', error);
-    }
-}
+		await channel.assertExchange(exchangeName, "direct");
+		channel.publish(exchangeName, routingKey, Buffer.from(message));
+	} catch (error) {
+		console.log("Publish direct message error", error);
+	}
+};
 
 module.exports = {
-    publishDirectMessage
-}
+	publishDirectMessage,
+};
